d9c404c67b7ed973c3c4c0aa547c6de71e5cea60
Francisca <al221811678@gmail.com>
2020-10-23 07:37:58 -0500

    videonueve

--- lib/src/pages/contador_page.dart
+++ lib/src/pages/contador_page.dart
@@ -12,28 +12,32 @@ class _ContadorPageState extends State<ContadorPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Stateful'),
-        centerTitle: true,
-      ),
-      body: Center(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text('Numero de taps:', style: _estiloTexto),
-          Text('$_conteo', style: _estiloTexto),
-        ],
-      )),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {
-          // print('hola mundo!');
+        appBar: AppBar(
+          title: Text('Stateful'),
+          centerTitle: true,
+        ),
+        body: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Numero de taps:', style: _estiloTexto),
+            Text('$_conteo', style: _estiloTexto),
+          ],
+        )),
+        floatingActionButton: _crearBotones());
+  }
 
-          setState(() {
-            _conteo++;
-          });
-        },
-      ),
+  Widget _crearBotones() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: <Widget>[
+        SizedBox(width: 30),
+        FloatingActionButton(child: Icon(Icons.exposure_zero), onPressed: null),
+        Expanded(child: SizedBox()),
+        FloatingActionButton(child: Icon(Icons.remove), onPressed: null),
+        SizedBox(width: 5.0),
+        FloatingActionButton(child: Icon(Icons.add), onPressed: null),
+      ],
     );
   }
 }
